eb06ea7198ec83f27cfc32193de4c2107741b367
jackrossvitha <54919586+jackrossvitha@users.noreply.github.com>
2019-11-14 19:18:30 -0700

    Add files via upload
    Must run both files to get the right database config

---
+++ CUThere_Update.sql
@@ -0,0 +1,62 @@
+drop table if exists `CUThere`.`eventDetails`;
+drop table if exists `CUThere`.`organizers`;
+drop table if exists `CUThere`.`locations`;
+drop table if exists `CUThere`.`users`;
+
+CREATE  TABLE IF NOT EXISTS `CUThere`.`eventDetails` (
+  `eventID` INT NOT NULL	,
+  `eventName` VARCHAR(50) NOT NULL ,
+  `dateOfEvent` DATE NOT NULL,
+  `timeStart` TIME NOT NULL,
+  `timeEnd` TIME NOT NULL,
+  `eventDescription` VARCHAR(500) NOT NULL,
+  `organizerID` INT,
+  `buildingID` INT NOT NULL,
+  `roomNumber` VARCHAR(4) NOT NULL,
+  PRIMARY KEY (`eventID`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `CUThere`.`organizers`(
+	`organizerID` INT NOT NULL,
+    `organizeName` VARCHAR(50) NOT NULL,
+    `publicEmail` VARCHAR(100),
+    `publicPhone` VARCHAR(13),
+    `pictureLink` VARCHAR(150),
+    PRIMARY KEY (`organizerID`)
+)
+ENGINE = InnoDB;
+
+INSERT INTO eventDetails (eventID, eventName, dateOfEvent, timeStart, timeEnd, eventDescription, organizerID, location, rsvp)
+VALUES (12345, 'Meeting4', '2019-10-17', '18:00:00', '20:00:00', 'Meeting 4 of Murphys Law', 54321, 'ITLL', TRUE);
+
+select * from eventDetails;
+
+create table if not exists `CUThere`.`locations`(
+		`buildingID` INT NOT NULL,
+		`buildingName` VARCHAR(100) NOT NULL,
+        `buildingNameAbbv` VARCHAR(4) NOT NULL,
+        PRIMARY KEY (`buildingID`)
+        )
+        ENGINE = InnoDB;
+        
+create table if not exists `CUThere`.`users`(
+    `userID` INT NOT NULL,
+    `userName` VARCHAR(35) NOT NULL,
+    `password` VARCHAR(16) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `DOB` DATE NOT NULL,
+    `firstName` VARCHAR(10) NOT NULL,
+    `lastName` VARCHAR(10) NOT NULL,
+    PRIMARY KEY (`userID`)
+	)
+ENGINE = InnoDB;
+	
+INSERT INTO locations (buildingID, buildingName, roomNumber, buildingNameAbbv)
+VALUES
+(1, 'Center for Community', 100, 'C4C'),
+(2, 'University Memorial Center', 100, 'UMC'),
+(3, 'Engineering Center', 100, 'ECCR'),
+(4, 'Koelbel Building', 100, 'KOBL'),
+(5, 'Eaton Humanities', 100, 'HUMN'); -- Testing values
+
+select * from locations;

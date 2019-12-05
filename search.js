671e5ee04ce4313397f4095004fa62f4bce872d3
ringkun <ringkun12@gmail.com>
2019-12-04 17:09:45 -0700

    Add files via upload

---
+++ search.js
@@ -0,0 +1,90 @@
+// function clearResult() {
+//   let ul = document.getElementById("myUL");
+//   $(ul).each(function(index) {
+//     ul.removeChild(this);
+//   });
+// }
+
+// function constructSearch(event) {
+//   clearResult();
+//   console.log("Construct events database");
+//   let ul = document.getElementById("myUL");
+//   if (event.length() == 0) {
+//     let a = document.getElementById("a");
+//     a.innerHTML = "We couldn't retrieve the events database";
+//     ul.appendChild(a);
+//     return;
+//   }
+//   for (i = 0; i < event.length; i++) {
+//     let li = document.createElement("li");
+//     ul.appendChild(li);
+//     let a = document.createElement("a");
+//     a.classList.add("results");
+//     a.innerHTML = event[i];
+//     console.log(event[i]);
+//     li.appendChild(a);
+//   }
+// }
+function toggleResult(r, open) {
+  if (r.className != "results") {
+    console.log(r.className + "Not Results class");
+    return;
+  }
+  if ((open && r.id == "open") || (!open && r.id == "close")) {
+    return;
+  }
+  if (open) {
+    r.style.display = "";
+    r.id = "open";
+    let state = 0;
+    var id = setInterval(frame, 1);
+    function frame() {
+      if (state >= 100) {
+        clearInterval(id);
+        // console.log("Opened");
+      } else {
+        state++;
+        r.style.width = state + "%";
+        r.style.height = 60 * (state / 100) + "px";
+      }
+    }
+    return;
+  } else {
+    r.id = "close";
+    let state = 0;
+    var id = setInterval(frame, 1);
+    function frame() {
+      if (state >= 100) {
+        r.style.display = "none";
+        clearInterval(id);
+        // console.log("Closed");
+      } else {
+        state++;
+        r.style.width = 100 - state + "%";
+        r.style.height = 60 * (1 - state / 100) + "px";
+      }
+    }
+    return;
+  }
+}
+
+function updateSearchResults(page) {
+  let filter = document.getElementById("myInput");
+  filter = filter.value;
+  // console.log(filter);
+  let reg = new RegExp(filter.toLowerCase());
+  let res = document.getElementsByClassName("results");
+  for (i = 0; i < res.length; i++) {
+    txt = res[i].innerHTML.toLowerCase();
+    match = txt.match(reg);
+    if (match != null) {
+      // console.log("open");
+      // toggleResult(res[i], true);
+      res[i].style.display = "";
+    } else {
+      // console.log("close");
+      // toggleResult(res[i], false);
+      res[i].style.display = "none";
+    }
+  }
+}
---
+++ searchserver.js
@@ -0,0 +1,52 @@
+const express = require("express");
+let app = express();
+const bodyparser = require("body-parser");
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+const ejs = require("ejs");
+const pgp = require("pg-promise")();
+const dbConfig = {
+  host: "localhost",
+  port: 5432,
+  database: "cuthere",
+  user: "postgres",
+  password: "wx111699"
+};
+let db = pgp(dbConfig);
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/")); // This line is necessary for us to use relative paths and access our resources directory
+
+app.use(express.static("public")); // This line is necessary for us to use relative paths and access our resources directory
+//Obtain event details
+app.get("/search", function(req, res) {
+  console.log(req.query.order);
+  let q = "";
+  let o = "";
+  if (req.query.order == "name" || req.query.order == null) {
+    q = "select * from eventDetails order by eventname";
+    o = "name";
+  } else {
+    q = "select * from eventDetails order by dateofevent,timestart";
+    o = "time";
+  }
+  db.task("get-everything", task => {
+    return task.batch([task.any(q)]);
+  })
+    .then(function(events) {
+      // console.log(events[0]);
+      res.render("search", {
+        event: events[0],
+        order: o,
+        error: ""
+      });
+    })
+    .catch(function(err) {
+      // display error message in case an error
+      res.render("search", {
+        error: "Events are missing"
+      });
+    });
+});
+
+app.listen(3000);
+console.log("Server complete");
